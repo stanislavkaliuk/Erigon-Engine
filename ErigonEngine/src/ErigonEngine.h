4683bc5f866b63619c7897f4441fd9ea5ca42505
Stanislav Kaliuk <staskalyuk.096@gmail.com>
2019-04-10 23:26:18 +0300

    Implement logger base init

--- ErigonEngine/src/ErigonEngine.h
+++ ErigonEngine/src/ErigonEngine.h
@@ -1,13 +1,17 @@
 #pragma once
 
+
+
+
 #include <stdio.h>
 
 //for use Erigon Engine Apps
 
 #include "ErigonEngine/Application.h"
-
+#include "ErigonEngine/Log.h"
 //---EntryPoint---
 
 #include "ErigonEngine/EntryPoint.h"
 
 //----------------
+
--- ErigonEngine/src/ErigonEngine/EntryPoint.h
+++ ErigonEngine/src/ErigonEngine/EntryPoint.h
@@ -6,7 +6,10 @@ extern ErigonEngine::Application* ErigonEngine::CreateApplication();
 
 int main(int argc,char** argv)
 {
-	printf("Helllowwww\n");
+	ErigonEngine::Log::Init();
+	ErigonEngine::Log::GetCoreLogger()->warn("Initialize Log!");
+	ErigonEngine::Log::GetClientLogger()->info("Initialize Client Log!");
+
 	auto app = ErigonEngine::CreateApplication();
 	app->Run();
 	delete app;
