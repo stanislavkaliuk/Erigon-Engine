3e6e4d41f3c0aecdeb5709fdf4a161b7c1fa40ac
Stanislav Kaliuk <staskalyuk.096@gmail.com>
2019-12-18 00:04:24 +0200

    SceneSystem: Add Scene Data structs, and define simple getters of data.

--- ErigonEngine/src/ErigonEngine/Tools/SceneSubsystem/SceneData.h
+++ ErigonEngine/src/ErigonEngine/Tools/SceneSubsystem/SceneData.h
@@ -1,22 +1,72 @@
 #pragma once
 #include <string>
 #include <vector>
-
-struct SceneItem
+#include <map>
+#include <variant>
+namespace ErigonEngine
 {
-	std::string name;
-	uint32_t gameObjectId;
-	std::vector<uint32_t> componentIds;
-};
+	namespace SceneManagement
+	{
+		struct GameObjectItem
+		{
+			std::string name;
+			uint32_t gameObjectId;
+			std::vector<ComponentItem> componentItems;
+		};
 
+		template<class... Types>
+		class ComponentParam
+		{
+		public:
+			std::string name;
+			Types value;
+		};
 
-class SceneData
-{
-public:
-	void AddSceneItem(const SceneItem& item);
-	void RemoveSceneItem(const SceneItem& item);
-	void RemoveSceneItem(const uint32_t id);
+		using FloatMap = std::map<std::string, float>;
+		using IntMap = std::map<std::string, uint32_t>;
+		using ComponentParamVector = std::vector<ComponentParam<std::variant<int, std::string, FloatMap, IntMap>>>;
 
-private:
-	std::vector<SceneItem> sceneItems;
-};
+		struct ComponentItem
+		{
+			std::string name;
+			uint32_t componentId;
+			ComponentParamVector componentParams;
+		};
+
+		class SceneData
+		{
+		public:
+			SceneData(const std::string& filePath);
+			void AddSceneItem(const GameObjectItem& item);
+			void RemoveSceneItem(const GameObjectItem& item);
+			void RemoveSceneItem(const uint32_t id);
+			void Save(const std::string& filePath);
+
+			GameObjectItem* GetGameObjectItem(int index)
+			{
+				if (index < sceneItems.size())
+				{
+					return &sceneItems[index];
+				}
+
+				return nullptr;
+			}
+
+			ComponentItem* GetComponentItem(const GameObjectItem& gameobjectItem, std::string& componentName)
+			{
+				for (auto component : gameobjectItem.componentItems)
+				{
+					if (component.name == componentName)
+					{
+						return &component;
+					}
+				}
+
+				return nullptr;
+			}
+
+		private:
+			std::vector<GameObjectItem> sceneItems;
+		};
+	}
+}
